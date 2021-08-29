7a12601de146e8276fb4f267d27b93e94db4aada
minami0720 <89454730+minami0720@users.noreply.github.com>
2021-08-29 12:35:56 +0900

    Add files via upload

--- schema.rb
+++ schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_23_064528) do
+ActiveRecord::Schema.define(version: 2021_08_29_012609) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,42 @@ ActiveRecord::Schema.define(version: 2021_08_23_064528) do
     t.string "category"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.string "image"
   end
 
+  create_table "lectures", force: :cascade do |t|
+    t.string "name"
+    t.string "username"
+    t.integer "overall"
+    t.integer "level"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "book_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id"], name: "index_likes_on_book_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.text "profile"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "likes", "books"
+  add_foreign_key "likes", "users"
 end
