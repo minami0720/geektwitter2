418cd6a9924ee58bdcb57caebeede59c88667e04
minami0720 <89454730+minami0720@users.noreply.github.com>
2021-08-29 12:26:40 +0900

    Add files via upload

--- routes.rb
+++ routes.rb
@@ -1,7 +1,21 @@
 Rails.application.routes.draw do
+  
+  devise_for :users
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  
-  get 'books/index' => 'books#index'
-  get 'books/new' => 'books#new'
-  root 'books#index'
+  get 'hello/index' => 'hello#index'
+
+  resources :books do
+    resources :likes, only: [:create, :destroy]
+
+root 'hello#index'
+
+end
+
+root 'lectures#index'
+get 'lectures/index'  => 'lectures#index'
+get 'lectures/new'  => 'lectures#new'
+post 'lectures/new'  => 'lectures#create'
+
 end
